ea82a97e98532f2b41cb693dd24df694c1e9a928
OvidiuGi <gireada_ovidiu@yahoo.com>
2022-03-08 11:23:44 +0200

    Fixed Room.php again commit ..

--- src/Entity/Room.php
+++ src/Entity/Room.php
@@ -28,28 +28,4 @@ class Room
 
         return $this;
     }
-
-    public function getStartTime() : \DateTime
-    {
-        return $this->startTime;
-    }
-
-    public function setStartTime(\DateTime $startTime): self
-    {
-        $this->startTime = $startTime;
-
-        return $this;
-    }
-
-    public function getEndTime() : \DateTime
-    {
-        return $this->endTime;
-    }
-
-    public function setEndTime(\DateTime $endTime): self
-    {
-        $this->endTime = $endTime;
-
-        return $this;
-    }
 }
