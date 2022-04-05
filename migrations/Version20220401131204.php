7c6458e0e8cd3b0ddf3f0a19d3ce6035f8afe187
OvidiuGi <gireada_ovidiu@yahoo.com>
2022-04-06 00:16:35 +0300

    Removed comments in migration

--- migrations/Version20220401131204.php
+++ migrations/Version20220401131204.php
@@ -19,14 +19,11 @@ final class Version20220401131204 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE user ADD deletedAt DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE `user`  DROP deletedAt');
-
     }
 }
